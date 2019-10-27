cca45943e318560717784e2de51aafa0e46bb436
patrisbane <52531415+patrisbane@users.noreply.github.com>
2019-10-27 15:17:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const randomDimensions = (num) => {
     ricardo.style.width = randomDimension(num);
     ricardo.style.height = randomDimension(num);
 }
-button.onwheel = function() {
+document.body.onwheel = function() {
     randomDimensions(500);
     randomColors();
 }
